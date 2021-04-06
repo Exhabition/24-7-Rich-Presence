4cdf44dbae209e38a53e4100a031f8f92b414a0d
Exhabition <lukeweerheim@hotmail.com>
2021-04-06 18:54:27 +0200

    Add basis code for supporting timestamp.end

--- logic/presence.js
+++ logic/presence.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 
 const { Client } = require('discord-rpc');
-const { developer, inviteUrl } = require('../configuration/config.json');
+const { developer, inviteUrl, timeLeft } = require('../configuration/config.json');
 
 const RPC = new Client({
     transport: "ipc",
 });
 
 RPC.on('ready', async () => {
-    console.info(`RP ready`);
+    console.info(`[RPC] Ready!`);
 });
 
 RPC.on("connected", async () => {
-    console.info(`RP connected`);
+    console.info(`[RPC] Connected!`);
 });
 
 RPC.login({
@@ -29,8 +29,9 @@ functions.updatePresence = function (songInfo, botInfo) {
     const botName = botInfo.name;
     const botIcon = botInfo.icon;
 
-    // Name and (optional) station of the song the user is listening too
+    // Name, length and (optional) station of the song the user is listening too
     const songName = songInfo.name;
+    const songLength = songInfo.length;
     const songStation = songInfo.station;
 
     // Log the song and what we are trying to update the status too
@@ -38,24 +39,39 @@ functions.updatePresence = function (songInfo, botInfo) {
     console.info(`[SONG TITLE] ${songName}`);
     console.info(`[SONG SOURCE] ${songStation}`);
 
+    // Check which setting is on, elapsed or remaining (we need to be able to access the length of the song if we want to use "remaining")
+    const timestampRequest = {};
+    if (timeLeft.toLowerCase() == "remaining" && songLength) {
+        timestampRequest["end"] = Date.now() + songLength;
+    } else {
+        timestampRequest["start"] = Date.now();
+    }
+
+    // Make an activity object so we can decide wether we use timestamps.start or timestamps.end
+    const activity = {
+        state: songStation && songName ? `${songName} | Audio Source: ${songStation}` : songName ? songName : songStation ? songStation : "Nothing",
+        details: "Listening to",
+        timestamps: {},
+        assets: {
+            large_image: botIcon,
+            large_text: botName,
+            small_image: "developer",
+            small_text: `Made by ${developer}`,
+        },
+        buttons: [{ label: `Invite`, url: inviteUrl },
+        {
+            label: 'Song on YouTube', url: 'https://www.youtube.com/results?search_query=' + (encodeURIComponent(songName ? songName : '')),
+        }],
+    };
+
+    // Apply timestamp setting
+    const typeOfTimestamp = Object.keys(timestampRequest)[0];
+    activity.timestamps[typeOfTimestamp] = timestampRequest[typeOfTimestamp];
+
     // Make a set activity request ourselves
     RPC.request('SET_ACTIVITY', {
         pid: process.pid,
-        activity: {
-            state: songStation && songName ? `${songName} | Audio Source: ${songStation}` : songName ? songName : songStation ? songStation : "Nothing",
-            details: "Listening to",
-            timestamps: {
-                start: Date.now(),
-            },
-            assets: {
-                large_image: botIcon,
-                large_text: botName,
-                small_image: "developer",
-                small_text: `Made by ${developer}`,
-            },
-            buttons: [{ label: `Invite`, url: inviteUrl },
-            { label: 'Song on YouTube', url: 'https://www.youtube.com/results?search_query=' + (encodeURIComponent(songName ? songName : '')) }],
-        },
+        activity: activity,
     }).catch(console.error);
 };
 
